e6ec01960dadc523f841b61c7630e2039bc9b54f
Daniel <233632326+Dani-red1@users.noreply.github.com>
2025-11-24 16:24:48 -0600

    Se agrega la carpeta Data y ReservationRepository para guardar las reservas y duracion de la reserva.

---
+++ Data/ReservationRepository.cs
@@ -0,0 +1,25 @@
+﻿using CampusTech.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CampusTech.Data
+{
+    public class ReservationRepository
+    {
+        private static List<Reservation> _reservations = new List<Reservation>();
+
+        public IEnumerable<Reservation> GetAll() => _reservations;
+
+        public string Add(Reservation r)
+        {
+            if (_reservations.Any(x => x.ReservationCode == r.ReservationCode))
+                return "El código de reserva ya existe.";
+
+            if (r.EndTime <= r.StartTime)
+                return "La hora de fin debe ser mayor a la hora de inicio.";
+
+            _reservations.Add(r);
+            return "OK";
+        }
+    }
+}
